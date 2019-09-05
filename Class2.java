d4d1c028feeb3bb4c2f476a2827be40c447403ff
Kyle Phillips <49827886+KyleP56@users.noreply.github.com>
2019-09-05 16:16:11 -0500

    Update Class2.java

--- Class2.java
+++ Class2.java
@@ -1,18 +1,12 @@
 /**
  *
- * @author Your Name
+ * @author Kyle Phillips
  */
 
 public class Class2 {
 
     private void f1() {
         // v1.0
-    }
-    private void f2() {
-        // v2.0
-    }
-    private void f3() {
-        // v3.0
     }
 
 }
